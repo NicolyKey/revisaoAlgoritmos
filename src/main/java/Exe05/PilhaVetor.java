07ff11dfdd03a2ece5114e06582b2056ba795288
Nicoly Araujo <nicoly.araujo@premiersoft.net>
2025-04-03 11:13:01 -0300

    exercicio 05

--- src/main/java/Exe05/PilhaVetor.java
+++ src/main/java/Exe05/PilhaVetor.java
@@ -68,13 +68,50 @@ public class PilhaVetor<T> implements Pilha<T> {
     String resultado = " ";
     
     for(int i =0; i< tamanho; i++){
-       resultado = info + ", ";
+       resultado = info[i] + ", ";
     }
         return resultado;
     }
     
-    
+    //    concatenar(PilhaVetor):este método deverá concatenar os dados da pilha fornecida como argumento (p) na
+    //pilha corrente . O novo topo da pilha deve ser igual ao topo de p. Após a operação, a pilha p deve permanecer com
+    //o mesmo conteúdo antes da invocação de concatenar(). Se a pilha corrente não tiver capacidade de armazenar
+    //todos os dados da pilha p, deve ser lançada uma exceção.
     public void concatenar(PilhaVetor<T> p){
+        if(p.getTamanho() + this.getTamanho() >= this.getLimite()){
+           throw new IndexOutOfBoundsException();
+        }
+        
+        T[] elementos =  (T[]) p.getInfo();
+        
+        for (int i = 0; i < p.getTamanho(); i++) {
+        this.push(elementos[i]);
+    }
+    }  
+
+    public Object[] getInfo() {
+        return info;
+    }
+
+    public void setInfo(Object[] info) {
+        this.info = info;
+    }
+
+    public int getLimite() {
+        return limite;
+    }
+
+    public void setLimite(int limite) {
+        this.limite = limite;
+    }
+
+    public int getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(int tamanho) {
+        this.tamanho = tamanho;
+    }
     
-    }    
+  
 }
