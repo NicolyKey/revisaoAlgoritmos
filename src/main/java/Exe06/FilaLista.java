e1715534f39fefa041617e4a3da78f237eba1d8e
Nicoly Keyssiane Lima Araujo <nklaraujo@furb.br>
2025-04-15 21:37:04 -0300

    atv Arvore

---
+++ src/main/java/Exe06/FilaLista.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Exe06;
+
+/**
+ *
+ * @author nklaraujo
+ */
+public class FilaLista<T> {
+    private ListaEncadeada<T> lista;
+
+    public FilaLista() {
+      lista = new ListaEncadeada<>();
+    }
+    //TODO: fazer a implementação da filaLista
+    
+    
+}
---
+++ src/main/java/Exe07/NoArvoreBinaria.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Exe07;
+
+/**
+ *
+ * @author nklaraujo
+ */
+public class NoArvoreBinaria<T> {
+    
+    private NoArvoreBinaria<T> esquerda;
+    private NoArvoreBinaria<T> direita;
+    private T info;
+
+    public NoArvoreBinaria(T info, NoArvoreBinaria<T> esquerda, NoArvoreBinaria<T> direita ) {
+        this.esquerda = esquerda;
+        this.direita = direita;
+        this.info = info;
+    }
+
+    public NoArvoreBinaria(T info) {
+        this.info = info;
+    }
+
+    public NoArvoreBinaria<T> getEsquerda() {
+        return esquerda;
+    }
+
+    public void setEsquerda(NoArvoreBinaria<T> esquerda) {
+        this.esquerda = esquerda;
+    }
+
+    public NoArvoreBinaria<T> getDireita() {
+        return direita;
+    }
+
+    public void setDireita(NoArvoreBinaria<T> direita) {
+        this.direita = direita;
+    }
+
+    public T getInfo() {
+        return info;
+    }
+
+    public void setInfo(T info) {
+        this.info = info;
+    }
+    
+    
+}
