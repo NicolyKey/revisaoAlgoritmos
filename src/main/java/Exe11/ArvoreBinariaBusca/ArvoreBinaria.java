c1c7975af64e6fe9944421d6a91bb2c4abd4502a
NicolyKey <nicolykeyssiane@gmail.com>
2025-06-08 18:03:46 -0300

    chore: ajustes na classe de arvore binaria

--- src/main/java/Exe11/ArvoreBinariaBusca/ArvoreBinaria.java
+++ src/main/java/Exe11/ArvoreBinariaBusca/ArvoreBinaria.java
@@ -8,21 +8,8 @@ public class ArvoreBinaria<T> extends ArvoreBinariaAbstract<T> {
         return null;
     }
 
-    //    Algoritmo: buscar(NoArvore no, int valor)
-//    se (no = null) então
-//    retornar null;
-//    fim-se
-//    se (valor = no.info) então
-//    retornar no;
-//    senão
-//    se (valor < no.info) então
-//    retornar buscar(no.esquerda, valor);
-//    senão
-//    retornar buscar(no.direita, valor);
-//    fim-se
-//    fim-se
     private  NoArvoreBinaria<T> buscar(NoArvoreBinaria<T> no, T valor){
-     return null;
+       return  null;
     }
 
     public void setRaiz(NoArvoreBinaria<T> raiz){
