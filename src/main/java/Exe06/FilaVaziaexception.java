15fb44821c935ad3b1b4346455722934f0fc98cd
Nicoly Araujo <nicoly.araujo@premiersoft.net>
2025-06-09 12:39:23 -0300

    feat: iniciando a estrutura do melhor de tres do quicksort

--- src/main/java/Exe06/FilaVaziaexception.java
+++ src/main/java/Exe06/FilaVaziaexception.java
@@ -1,28 +1,4 @@
 package Exe06;
 
 public class FilaVaziaException  extends RuntimeException{
-
-    //    Sendo que:
-//            • FilaVetor(int):
-//    Construtor que deve inicializar a fila com capacidade de armazenamento igual ao valor do parâmetro.
-//• inserir(T)
-//    Este método deve enfileirar o dado fornecido como argumento. Caso a fila não tenha espaço disponível, o método
-//    deverá lançar a exceção FilaCheiaException.
-//• estaVazia():boolean
-//    Este método deve avaliar se a fila está vazia ou não, retornando true se a fila estiver vazia e false se não estiver.
-//• peek():T
-//    Este método deve retornar o dado que estiver armazenado no inicio da fila. Caso a fila esteja vazia, o método
-//    deverá lançar a exceção FilaVaziaException.
-//• retirar():T
-//    Este método deve desenfileirar um dado da fila. Caso a fila esteja vazia, o método deverá lançar a exceção
-//    FilaVaziaException.
-//• liberar()
-//    O método liberar deve desenfileirar todos os dados da fila.
-//            • criarFilaConcatenada(f2: FilaVetor): FilaVetor
-//    Este método deve criar uma nova fila, a partir da concatenação de duas filas previamente existentes: a fila do
-//    objeto que executar o método criarFilaConcatenada(),aqui denominada de f1, e a fila recebida como
-//    argumento, denominada de f2. Observe a ilustração abaixo, que apresenta duas filas originais e seus elementos
-//    corretamente inseridos numa nova fila resultante (f3)
-
-
 }
