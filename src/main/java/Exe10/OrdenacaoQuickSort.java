5e2ffeaf4bae919539806f97f47773e52b1f95f2
Nicoly Keyssiane Lima Araujo <nklaraujo@furb.br>
2025-06-10 18:51:29 -0300

    feat: fazendo metodo melhor de tres do quicksort

--- src/main/java/Exe10/OrdenacaoQuickSort.java
+++ src/main/java/Exe10/OrdenacaoQuickSort.java
@@ -6,39 +6,60 @@ public class OrdenacaoQuickSort <T extends Comparable<T>> extends OrdenacaoAbstr
         quickSort(0,n);
     }
 
-    private void quickSort(int inicio, int fim){
-        if(inicio < fim){
-            int idxPivo = particionar(inicio, fim);
-            quickSort(inicio, idxPivo - 1);
-            quickSort(idxPivo + 1, fim);
+        private void quickSort(int inicio, int fim){
+            if(inicio < fim){
+                int idxPivo = particionar(inicio, fim);
+                quickSort(inicio, idxPivo - 1);
+                quickSort(idxPivo + 1, fim);
+            }
         }
-    }
 
-    private int particionar(int inicio, int fim) {
-        T[] info = getInfo();
-        int a = inicio;
-        int b = fim + 1;
-        // TODO: adicionar aqui a parte do melhor de três do quicksort
-        T pivo = info[inicio];
+            private int particionar(int inicio, int fim) {
+            T[] info = getInfo();
+            T pivo = selecionarPivo(inicio, fim); 
+            int a = inicio;
+            int b = fim;
 
-        while (true) {
-            do {
-                a++;
-            } while (a <= fim && info[a].compareTo(pivo) < 0);
-
-            do {
-                b--;
-            } while (b >= inicio && info[b].compareTo(pivo) > 0);
-
-            if (a >= b) {
-                break;
+            while (a <= b) {
+                while (info[a].compareTo(pivo) < 0) {
+                    a++;
+                }
+                while (info[b].compareTo(pivo) > 0) {
+                    b--;
+                }
+                if (a <= b) {
+                    trocar(a, b);
+                    a++;
+                    b--;
+                }
             }
 
-            trocar(a, b);
+            return a;
         }
 
-        trocar(inicio, b);
+        private T selecionarPivo(int inicio, int fim) {
+            int meio = calcularIndiceMeio(inicio, fim);
+            ordenarTresElementos(inicio, meio, fim); // Garante que info[meio] é a mediana
+            trocar(meio, inicio); // Move a mediana para o início
+            return getInfo()[inicio];
+        }
 
-        return b;
-    }
+        private int calcularIndiceMeio(int inicio, int fim) {
+            return (inicio + fim) / 2;
+        }
+
+        private void ordenarTresElementos(int i, int j, int k) {
+            T[] info = getInfo();
+
+            if (info[i].compareTo(info[j]) > 0) {
+                trocar(i, j);
+            }
+            if (info[i].compareTo(info[k]) > 0) {
+                trocar(i, k);
+            }
+            if (info[j].compareTo(info[k]) > 0) {
+                trocar(j, k);
+            }
+        }
+
 }
