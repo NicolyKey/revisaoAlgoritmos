cc3bf778e5c85cf3045e18e9e110a3c65e9d0f98
Nicoly Keyssiane Lima Araujo <nklaraujo@furb.br>
2025-06-10 21:29:48 -0300

    feat: fazendo o método de retirar porem precisa analisar os casos 2 e 3 pois esta incompleto

--- src/main/java/Exe11/ArvoreBinariaBusca/ArvoreBinariaBusca.java
+++ src/main/java/Exe11/ArvoreBinariaBusca/ArvoreBinariaBusca.java
@@ -7,21 +7,92 @@ public class ArvoreBinariaBusca<T extends Comparable<T>> extends ArvoreBinariaAb
 //    Negativo: O objeto atual é menor.
 //    Zero: Os objetos são iguais.
 //    Positivo: O objeto atual é maior.
-    
     @Override
     public NoArvoreBinaria<T> buscar(T valor) {
         NoArvoreBinaria<T> p = this.getRaiz();
         // o compare é mais custoso que o equals 
-        while(p != null && p.getInfo().compareTo(valor) != 0){
-            if(valor.compareTo(p.getInfo()) < 0){
+        while (p != null && p.getInfo().compareTo(valor) != 0) {
+            if (valor.compareTo(p.getInfo()) < 0) {
                 p = p.getEsquerda();
-            }else{
+            } else {
                 p = p.getDireita();
             }
         }
         return p;
     }
 
+    public void retirar(T valor) {
+        NoArvoreBinaria<T> ponteiro = this.getRaiz();
+        NoArvoreBinaria<T> pai = null;
+        boolean filhoEsquerdo = false;
+
+        while (ponteiro != null && !ponteiro.getInfo().equals(valor)) {
+            pai = ponteiro;
+            if (valor.compareTo(ponteiro.getInfo()) < 0) {
+                filhoEsquerdo = true;
+                ponteiro = ponteiro.getEsquerda();
+            } else {
+                filhoEsquerdo = false;
+                ponteiro = ponteiro.getDireita();
+            }
+        }
+
+        if (ponteiro != null) {
+            if (ponteiro.getEsquerda() == null && ponteiro.getDireita() == null) {
+                if (ponteiro.equals(getRaiz())) {
+                    setRaiz(null);
+                } else {
+                    if (filhoEsquerdo) {
+                        pai.setEsquerda(null);
+                    } else {
+                        pai.setDireita(null);
+                    }
+                }
+            } else {
+
+                if (ponteiro.getDireita() == null) {
+                    if (ponteiro == getRaiz()) {
+                        setRaiz(ponteiro.getEsquerda());
+                    } else {
+                        if (filhoEsquerdo) {
+                            pai.setEsquerda(ponteiro.getDireita());
+                        } else {
+                            pai.setDireita(ponteiro.getDireita());
+                        }
+                    }
+                    
+                } else {
+                    NoArvoreBinaria<T> sucessor = extrairSucessor(ponteiro);
+
+                    if (ponteiro == getRaiz()) {
+                        setRaiz(sucessor);
+                    } else {
+                        if (filhoEsquerdo) {
+                            pai.setEsquerda(sucessor);
+                        } else {
+                            pai.setDireita(sucessor);
+                        }
+                        sucessor.setEsquerda(ponteiro.getEsquerda());
+                    }
+                }
+            }
+        }
+
+        // só vai cair aqui se caso o ponteiro for nulo
+    }
+
+    public NoArvoreBinaria<T> extrairSucessor(NoArvoreBinaria<T> ponteiro) {
+        NoArvoreBinaria<T> atual = ponteiro.getDireita();
+        NoArvoreBinaria<T> paiSucessor = ponteiro;
+        NoArvoreBinaria<T> sucessor = ponteiro;
+
+        while (atual != null) {
+            paiSucessor = sucessor;
+            sucessor = atual;
+            atual = atual
+        }
+
+    }
 
     public void inserir(T info) {
         NoArvoreBinaria<T> novoNo = new NoArvoreBinaria<>(info);
@@ -77,7 +148,9 @@ public class ArvoreBinariaBusca<T extends Comparable<T>> extends ArvoreBinariaAb
     }
 
     private boolean estaBalanceada(NoArvoreBinaria<T> no) {
-        if (no == null) return true;
+        if (no == null) {
+            return true;
+        }
 
         int alturaEsq = altura(no.getEsquerda());
         int alturaDir = altura(no.getDireita());
@@ -90,7 +163,9 @@ public class ArvoreBinariaBusca<T extends Comparable<T>> extends ArvoreBinariaAb
     }
 
     private int altura(NoArvoreBinaria<T> no) {
-        if (no == null) return 0;
+        if (no == null) {
+            return 0;
+        }
         return 1 + Math.max(altura(no.getEsquerda()), altura(no.getDireita()));
     }
 
@@ -118,7 +193,5 @@ public class ArvoreBinariaBusca<T extends Comparable<T>> extends ArvoreBinariaAb
 
         return true;
     }
-
-
 
 }
